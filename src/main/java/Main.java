aa9ab656d6734a1920ccd7314279542a5f55b116
Catarina Trancoso <catarinamftrancoso@gmail.com>
2026-01-05 20:37:00 +0000

    updates files location, according to website

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,8 @@
 import katas.LeetCode.RemoveElement;
-import katas.LeetCode.TestStudy;
 
 public class Main {
     public static void main(String[] args) {
         RemoveElement actual = new RemoveElement();
-        TestStudy testStudy = new TestStudy();
         int[] one = {0,1,1,2,2,4};
         int[] two = {0,1,1,2,2,4};
         int[] three = {0,1,2,2,3,0,4,2};
@@ -12,9 +10,7 @@ public class Main {
         int[] nums = {3,3};
 
         actual.swapWithLast(one, 2);
-        testStudy.removeElement(two, 2);
         actual.swapWithLast(three, 3);
-        testStudy.removeElement(four, 3);
 
     }
 
